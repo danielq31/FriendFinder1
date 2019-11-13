ead8430008a67b5b8373560e11e00f65aa525542
daniel quintero <danielq1143@gmail.com>
2019-11-13 13:20:52 -0800

    finsished routes folder

---
+++ app/routes/apiRoute.js
@@ -0,0 +1,80 @@
+
+var userData = require("../data/friends");
+
+
+module.exports = function(app) {
+  
+  app.get("/api/friends", function(req, res) {
+    res.json(userData);
+  });
+
+  var comparisonUserTotalScore = 0;
+
+  var friendScores = [];
+
+
+
+  app.post("/api/friends", function(req, res) {
+
+    // Store current user scores in array.
+    var currentUserScores = req.body.scores;
+
+    console.log("Current user scores: " + currentUserScores);
+
+   
+    for (var i = 0; i < userData.length; i++) {
+
+    
+      var comparisonUserScores = userData[i].scores;
+
+      
+      comparisonUserTotalScore = calculateUserCompatibilityScore(currentUserScores, comparisonUserScores);
+
+      
+      friendScores.push(comparisonUserTotalScore);
+
+    }
+
+    console.log("Array of friend scores: " + friendScores);
+
+    var index = 0;
+    var value = friendScores[0];
+
+    
+    for (var i = 0; i < friendScores.length; i++) {
+      console.log("Value of item in array: " + friendScores[i]);
+      if (friendScores[i] < value) {
+        value = friendScores[i];
+        index = i;
+      }
+    }
+
+    
+    console.log("Best friend name: " + userData[index].name);
+
+    
+    res.send(userData[index]);
+
+   
+    userData.push(req.body);
+
+  });
+};
+
+var totalDifference = 0;
+
+
+function calculateUserCompatibilityScore(currentUserScores, comparisonUserScores) {
+
+  // Reset the total difference counter each time function called.
+  totalDifference = 0;
+
+  for (var i = 0; i < currentUserScores.length; i++) {
+
+    totalDifference+=Math.abs(currentUserScores[i] - comparisonUserScores[i]);
+  }
+
+  console.log("Final total difference for friend: " + totalDifference);
+
+  return totalDifference;
+};
